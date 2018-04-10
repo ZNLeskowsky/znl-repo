bbf9a56cc09031f8e2ce76f46be901d962c1a877
Zoltan Leskowsky <zleskowsky@acm.org>
2018-04-10 19:41:30 -0400

    Fixed make_*.sh scripts

--- make_mpscqueue_test.sh
+++ make_mpscqueue_test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
-c++ -std=c++11 -pthread -o mpscqueue_test src/mpscqueue_test.cpp 2>&1 |tee make_mpscqueue_test.out
-#c++ -std=c++11 -Wc++1z-extensions -pthread -o mpscqueue_test src/mpscqueue_test.cpp 2>&1 |tee make_mpscqueue_test.out
+SRC="$( cd "$( dirname $0 )" && pwd )"
+c++ -std=c++11 -pthread -o mpscqueue_test $SRC/mpscqueue_test.cpp 2>&1 |tee make_mpscqueue_test.out
+#c++ -std=c++11 -Wc++1z-extensions -pthread -o mpscqueue_test $SRC/mpscqueue_test.cpp 2>&1 |tee make_mpscqueue_test.out
--- make_taskqueue_test.sh
+++ make_taskqueue_test.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-c++ -std=c++11 -pthread -o taskqueue_test src/worker.cpp src/actor.cpp src/logger.cpp src/taskqueue_test.cpp 2>&1 |tee make_taskqueue_test.out
+SRC="$( cd "$( dirname $0 )" && pwd )"
+c++ -std=c++11 -pthread -o taskqueue_test $SRC/worker.cpp $SRC/actor.cpp $SRC/logger.cpp $SRC/taskqueue_test.cpp 2>&1 |tee make_taskqueue_test.out
